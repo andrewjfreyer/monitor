b1452b99e0aafc940fcbaab75fc20e2fc14ccd6e
Andrew J Freyer <andrew.freyer@gmail.com>
2018-05-22 17:03:31 -0600

    0.1.67

--- monitor.sh
+++ monitor.sh
@@ -26,7 +26,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-version=0.1.66
+version=0.1.67
 
 #CYCLE BLUETOOTH INTERFACE 
 sudo hciconfig hci0 down && sudo hciconfig hci0 up
@@ -373,7 +373,7 @@ public_device_scanner &
 
 
 #TRAP EXIT FOR CLEANUP ON OLDER INSTALLATIONS
-trap "sudo rm main_pipe &>/dev/null; sudo rm scan_pipe &>/dev/null; while read line; do `sudo kill $line`;done < <(ps ax | grep monitor.sh | awk '{print $1}')" EXIT
+trap "while read line; do $(sudo kill $line); done < <(ps ax | grep monitor.sh | awk '{print $1}'); sudo rm main_pipe &>/dev/null; sudo rm scan_pipe &>/dev/null" EXIT
 
 # ----------------------------------------------------------------------------------------
 # SCAN NEXT DEVICE IF REQUIRED
