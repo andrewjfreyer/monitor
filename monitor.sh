6ce71d8e8c7b3eec0c5ad0e33f2e8f9d319226b2
Andrew J Freyer <andrew.freyer@gmail.com>
2018-06-02 14:21:08 -0600

    0.1.173

--- monitor.sh
+++ monitor.sh
@@ -26,7 +26,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-version=0.1.172
+version=0.1.173
 
 # ----------------------------------------------------------------------------------------
 # PRETTY PRINT FOR DEBUG
@@ -540,6 +540,10 @@ refresh_global_states() {
 # ARRIVAL SCAN 
 # ----------------------------------------------------------------------------------------
 arrival_scan () {
+	#DO NOT SCAN IF ALL DEVICES ARE PRESENT
+	[ "$all_present" ] && return 0 
+
+	#ITERATE THROUGH THE KNOWN DEVICES 
 	for known_addr in "${!known_static_addresses[@]}"; do 
 		#GET STATE; ONLY SCAN FOR ARRIVED DEVICES
 		local state=known_device_log[$known_addr]
@@ -649,12 +653,12 @@ while true; do
 
 			if [ "$mqtt_instruction" == "ARRIVE" ]; then 
 				#SET SCAN TYPE
-				next_scan_type="ARRIVAL_SCAN"
+				arrival_scan
 
 			elif [ "$mqtt_instruction" == "DEPART" ]; then 
 
 				#SET SCAN TYPE
-				next_scan_type="DEPARTURE_SCAN"
+				departure_scan
 
 			else 
 				#SET SCAN TYPE
@@ -837,8 +841,9 @@ while true; do
 
 		elif [ "$cmd" == "RAND" ] && [ "$is_new" == true ] ; then 
 			log "${RED}[CMD-$cmd]${NC}	$data $pdu_header $name RAND_NUM: ${#random_device_log[@]}"
-			next_scan_type="ARRIVAL_SCAN"
-			random_device_last_update=$(date +%s)
+			
+			#TRIGGER ARRIVAL SCAN 
+			arrival_scan
 		fi 
 
 	done < main_pipe
