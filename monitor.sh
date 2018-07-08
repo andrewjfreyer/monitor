3c5fa08b2d31fe6a12763734c657377445659cb5
Andrew J Freyer <andrew.freyer@gmail.com>
2018-07-08 10:26:06 -0600

    0.1.425

--- monitor.sh
+++ monitor.sh
@@ -113,7 +113,7 @@ declare -A device_expiration_biases
 last_arrival_scan=$(date +%s)
 last_depart_scan=$(date +%s)
 
-echo "$LINENO"
+echo "$LINENO" >&2
 
 # ----------------------------------------------------------------------------------------
 # POPULATE THE ASSOCIATIVE ARRAYS THAT INCLUDE INFORMATION ABOUT THE STATIC DEVICES
@@ -123,12 +123,12 @@ echo "$LINENO"
 #LOAD PUBLIC ADDRESSES TO SCAN INTO ARRAY, IGNORING COMMENTS
 known_static_addresses=($(sed 's/#.\{0,\}//g' < "$PUB_CONFIG" | awk '{print $1}' | grep -oiE "([0-9a-f]{2}:){5}[0-9a-f]{2}" ))
 
-echo "$LINENO"
+echo "$LINENO" >&2
 
 
 #POPULATE KNOWN DEVICE ADDRESS
 for addr in ${known_static_addresses[@]}; do 
-echo "$LINENO"
+echo "$LINENO" >&2
 
 	#WAS THERE A NAME HERE?
 	known_name=$(grep "$addr" "$PUB_CONFIG" | tr "\\t" " " | sed 's/  */ /g;s/#.\{0,\}//g' | sed "s/$addr //g;s/  */ /g" )
@@ -137,7 +137,7 @@ echo "$LINENO"
 	[ ! -z "$known_name" ] && known_static_device_name[$addr]="$known_name"
 done
 
-echo "$LINENO"
+echo "$LINENO" >&2
 
 # ----------------------------------------------------------------------------------------
 # ASSEMBLE ARRIVAL SCAN LIST
@@ -145,7 +145,7 @@ echo "$LINENO"
 
 scannable_devices_with_state () {
 
-	echo "$LINENO"
+	echo "$LINENO" >&2
 
 	#DEFINE LOCAL VARS
 	local return_list
@@ -170,7 +170,7 @@ scannable_devices_with_state () {
 		scan_type_diff=$((timestamp - last_arrival_scan))
 	fi 
 
-	echo "$LINENO"
+	echo "$LINENO" >&2
 
 	#REJECT IF WE SCANNED TO RECENTLY
 	[ "$scan_type_diff" -lt "10" ] && log "${RED}[REJECT]	${GREEN}**** Rejected repeat scan. **** ${NC}" && return 0
@@ -180,13 +180,13 @@ scannable_devices_with_state () {
 	#SCAN ALL? SET THE SCAN STATE TO [X]
 	[ -z "$scan_state" ] && scan_state=2
 
-	echo "$LINENO"
+	echo "$LINENO" >&2
 
 			 	
 	#ITERATE THROUGH THE KNOWN DEVICES 
 	for known_addr in "${known_static_addresses[@]}"; do 
 
-	echo "$LINENO"
+	echo "$LINENO" >&2
 
 		
 		#GET STATE; ONLY SCAN FOR DEVICES WITH SPECIFIC STATE
@@ -219,7 +219,7 @@ scannable_devices_with_state () {
 		fi 
 	done
 
-	echo "$LINENO"
+	echo "$LINENO" >&2
 
  
 	#RETURN LIST, CLEANING FOR EXCESS SPACES OR STARTING WITH SPACES
@@ -229,7 +229,7 @@ scannable_devices_with_state () {
 	echo "$return_list"
 }
 
-echo "$LINENO"
+echo "$LINENO" >&2
 
 # ----------------------------------------------------------------------------------------
 # SCAN FOR DEVICES
@@ -377,7 +377,7 @@ perform_complete_scan () {
 
 }
 
-echo "$LINENO"
+echo "$LINENO" >&2
 
 
 # ----------------------------------------------------------------------------------------
@@ -407,7 +407,7 @@ perform_departure_scan () {
 	fi
 }
 
-echo "$LINENO"
+echo "$LINENO" >&2
 
 perform_arrival_scan () {
 
@@ -432,7 +432,7 @@ perform_arrival_scan () {
 	fi 
 }
 
-echo "$LINENO"
+echo "$LINENO" >&2
 
 # ----------------------------------------------------------------------------------------
 # ADD AN ARRIVAL SCAN INTO THE QUEUE 
@@ -440,13 +440,13 @@ echo "$LINENO"
 
 #ONLY SCAN IF NOT ON TRIGGER MODE
 if [ "$PREF_TRIGGER_MODE" == false ]; then 
-	echo "$LINENO"
+	echo "$LINENO" >&2
 
 	first_arrive_list=$(scannable_devices_with_state 0)
-	echo "$LINENO"
+	echo "$LINENO" >&2
 
 	perform_complete_scan "$first_arrive_list" "$PREF_ARRIVAL_SCAN_ATTEMPTS" &
-	echo "$LINENO"
+	echo "$LINENO" >&2
 
 	scan_pid=$!
 	scan_type=0
@@ -456,14 +456,14 @@ fi
 # LAUNCH BACKGROUND PROCESSES
 # ----------------------------------------------------------------------------------------
 
-echo "$LINENO"
+echo "$LINENO" >&2
 log_listener &
 btle_scanner & 
 mqtt_listener &
 btle_listener &
 periodic_trigger & 
 refresh_databases &
-echo "$LINENO"
+echo "$LINENO" >&2
 
 # ----------------------------------------------------------------------------------------
 # MAIN LOOPS. INFINITE LOOP CONTINUES, NAMED PIPE IS READ INTO SECONDARY LOOP
@@ -471,11 +471,11 @@ echo "$LINENO"
 
 #MAIN LOOP
 while true; do 
-	echo "$LINENO"
+	echo "$LINENO" >&2
 
 	#READ FROM THE MAIN PIPE
 	while read event; do 
-		echo "$LINENO"
+		echo "$LINENO" >&2
 		#DIVIDE EVENT MESSAGE INTO TYPE AND DATA
 		cmd="${event:0:4}"
 		data="${event:4}"
