1dbc640e7df4de7892a61a21aa6358106bcaa7d7
Andrew J Freyer <andrew.freyer@gmail.com>
2018-09-08 16:00:42 -0600

    0.1.616

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-version=0.1.615
+version=0.1.616
 
 #CAPTURE ARGS IN VAR TO USE IN SOURCED FILE
 RUNTIME_ARGS="$@"
