b98e57f5676e661cfdd5fcadf7400731001f429f
Andrew J Freyer <andrew.freyer@gmail.com>
2018-09-25 17:33:20 -0600

    Beta Branch Init

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-version=0.1.662
+version=0.1.663
 
 #CAPTURE ARGS IN VAR TO USE IN SOURCED FILE
 RUNTIME_ARGS="$@"
