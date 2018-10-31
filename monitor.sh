3790a69229ef8280eea348498d2129c7c4a6be26
Andrew J Freyer <andrew.freyer@gmail.com>
2018-10-31 13:43:54 -0600

    0.1.700
    • Update cache handling again

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-version=0.1.699
+version=0.1.700
 
 #CAPTURE ARGS IN VAR TO USE IN SOURCED FILE
 RUNTIME_ARGS="$@"
