e349ec4734d851afcea8116722b5131f5e861b35
Andrew J Freyer <andrew.freyer@gmail.com>
2018-11-02 10:30:19 -0600

    0.1.706
    • Announce online with every mqtt message sent from client; should cure
    homeassistant configurations with status requirements

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-version=0.1.705
+version=0.1.706
 
 #CAPTURE ARGS IN VAR TO USE IN SOURCED FILE
 RUNTIME_ARGS="$@"
