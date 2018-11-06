a5e0ca11e58b5f59cc1cce3c35bd1c1ac1679414
Andrew J Freyer <andrew.freyer@gmail.com>
2018-11-06 13:50:38 -0700

    0.1.711
    • Continued updates to BTLE support

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-version=0.1.710
+version=0.1.711
 
 #CAPTURE ARGS IN VAR TO USE IN SOURCED FILE
 RUNTIME_ARGS="$@"
