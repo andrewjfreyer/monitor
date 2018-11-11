32d74609e874045c0dbe9f7e94b6257a5ad28e98
Andrew J Freyer <andrew.freyer@gmail.com>
2018-11-11 07:31:34 -0700

    0.1.712
    • Improved version number

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-version=0.1.711
+version=0.1.712
 
 #CAPTURE ARGS IN VAR TO USE IN SOURCED FILE
 RUNTIME_ARGS="$@"
