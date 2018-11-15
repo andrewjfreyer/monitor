1c2b95716a331673cc72b4ea40b830fe81341b6a
Andrew J Freyer <andrew.freyer@gmail.com>
2018-11-15 16:26:13 -0700

    0.1.729
    • Update default settings

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-version=0.1.728
+version=0.1.729
 
 #CAPTURE ARGS IN VAR TO USE IN SOURCED FILE
 RUNTIME_ARGS="$@"
