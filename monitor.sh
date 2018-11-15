d0ecf46217605b1db2747878a969527d4d4144e3
Andrew J Freyer <andrew.freyer@gmail.com>
2018-11-15 16:42:13 -0700

    0.1.731
    • Minor bug correction to BTLE public reporting & RSSI filtering

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-version=0.1.730
+version=0.1.731
 
 #CAPTURE ARGS IN VAR TO USE IN SOURCED FILE
 RUNTIME_ARGS="$@"
