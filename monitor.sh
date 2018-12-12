c810cd0ba6898be04215130417f5517f84c35c24
Andrew J Freyer <andrew.freyer@gmail.com>
2018-12-12 10:35:04 -0700

    0.1.749
    • Attempted correction of 100% CPU usage (may be related to process
    mgmt and packet processing… working theory)

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-export version=0.1.748
+export version=0.1.749
 
 #CAPTURE ARGS IN VAR TO USE IN SOURCED FILE
 export RUNTIME_ARGS=("$@")
