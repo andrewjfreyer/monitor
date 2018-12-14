b17a79168eca1a42e89707c722e909d0ec8746d3
Andrew J Freyer <andrew.freyer@gmail.com>
2018-12-14 14:56:12 -0700

    0.1.755
    • [TEST RELEASE] consume oem data and change default behaviors to
    improve arrive/depart timing

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-export version=0.1.754
+export version=0.1.755
 
 #CAPTURE ARGS IN VAR TO USE IN SOURCED FILE
 export RUNTIME_ARGS=("$@")
