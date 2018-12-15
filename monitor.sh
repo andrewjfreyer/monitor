932598dd1200df597beef8e48b1b404fd3544fe0
Andrew J Freyer <andrew.freyer@gmail.com>
2018-12-14 19:43:18 -0700

    0.1.760

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-export version=0.1.759
+export version=0.1.760
 
 #CAPTURE ARGS IN VAR TO USE IN SOURCED FILE
 export RUNTIME_ARGS=("$@")
