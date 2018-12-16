8ec5303096d7dfc2868f86b574604291583472d6
Andrew J Freyer <andrew.freyer@gmail.com>
2018-12-16 15:19:59 -0700

    0.1.770
    • [TEST RELEASE] Dramatic increase of timeout delays to prevent
    accumulated overbuffering/overallocation

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-export version=0.1.768
+export version=0.1.770
 
 #CAPTURE ARGS IN VAR TO USE IN SOURCED FILE
 export RUNTIME_ARGS=("$@")
