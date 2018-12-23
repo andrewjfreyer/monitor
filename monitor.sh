e8640585851e3935046533a7936bcc0ba571cb25
Andrew J Freyer <andrew.freyer@gmail.com>
2018-12-22 16:24:15 -0800

    0.1.771
    • (TEST) Update version number

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-export version=0.1.770
+export version=0.1.771
 
 #CAPTURE ARGS IN VAR TO USE IN SOURCED FILE
 export RUNTIME_ARGS=("$@")
