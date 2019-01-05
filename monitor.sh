e232305cf6f3b04d79138fe74120727611b51694
Andrew J Freyer <andrew.freyer@gmail.com>
2019-01-05 11:31:05 -0700

    0.1.779
    • Remove latent variables from depreciated features
    • update connection argv handling

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-export version=0.1.778
+export version=0.1.779
 
 #CAPTURE ARGS IN VAR TO USE IN SOURCED FILE
 export RUNTIME_ARGS=("$@")
