b54d5b11dacdc0dfec1bbd9e28e63eb279f96cd5
Andrew J Freyer <andrew.freyer@gmail.com>
2019-01-05 13:22:01 -0700

    0.1.781
    • Suppress logging output from bluetoothctl

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-export version=0.1.780
+export version=0.1.781
 
 #CAPTURE ARGS IN VAR TO USE IN SOURCED FILE
 export RUNTIME_ARGS=("$@")
