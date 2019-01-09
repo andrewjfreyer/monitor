7f9101ebbe65fce17023ba22389fb3d86975c13b
Andrew J Freyer <andrew.freyer@gmail.com>
2019-01-09 11:20:02 -0700

    0.1.816
    • Update log_pipe for use with printf in lieu of echo

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-export version=0.1.815
+export version=0.1.816
 
 #CAPTURE ARGS IN VAR TO USE IN SOURCED FILE
 export RUNTIME_ARGS=("$@")
