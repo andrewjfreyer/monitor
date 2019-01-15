ebd1dfd4b9173e9eb705dd3d33311b675c095a44
Andrew J Freyer <andrew.freyer@gmail.com>
2019-01-14 23:32:21 -0700

    0.1.836
    • BTLE spacing fixes

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-export version=0.1.835
+export version=0.1.836
 
 #CAPTURE ARGS IN VAR TO USE IN SOURCED FILE
 export RUNTIME_ARGS=("$@")
