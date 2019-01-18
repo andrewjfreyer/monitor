0698a98a1fb3b02117acc0f4f17ed4cc0bb1ee08
Andrew J Freyer <andrew.freyer@gmail.com>
2019-01-17 19:31:01 -0700

    0.1.848
    • [TESTING]

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,8 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-export version=0.1.847
+export version=0.1.848
+printf "!!! this is a BETA test release !!!"
 
 #CAPTURE ARGS IN VAR TO USE IN SOURCED FILE
 export RUNTIME_ARGS=("$@")
