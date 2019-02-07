d27a8a918869bee10de5a7b786c8ee65f6b5feb3
Andrew J Freyer <andrew.freyer@gmail.com>
2019-02-07 08:37:44 -0700

    0.1.977
    • Test release for beacon expiration improvements

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-export version=0.1.976
+export version=0.1.977
 
 #COLOR OUTPUT FOR RICH OUTPUT 
 ORANGE=$'\e[1;33m'
@@ -904,6 +904,8 @@ while true; do
 				cmd="PUBL"
 				unset "random_device_log[$mac]"
 
+				log "[CMD-INFO]	Converting RAND $mac to PUBL $mac ($LINENO)"
+
 				#BEACON TYPE
 				beacon_type="GENERIC_BEACON_RANDOM"
 
@@ -918,9 +920,9 @@ while true; do
 			else
 
 				#IS THIS ALREADY IN THE STATIC LOG? 
-				if [ -n  "${public_device_log[$mac]}" ]; then
-
-					log "[CMD-INFO]	Converting RAND $mac to PUBL $mac"
+				if [ ${public_device_log[$mac]+true} ]; then
+					
+					log "[CMD-INFO]	Converting RAND $mac to PUBL $mac ($LINENO)"
 
 					#IS THIS A NEW STATIC DEVICE?
 					public_device_log[$mac]="$timestamp"
