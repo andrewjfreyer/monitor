32eafa73dac17a44fb73ac4594804fd14dacb547
Andrew J Freyer <andrew.freyer@gmail.com>
2019-02-07 16:03:56 -0700

    0.1.981
    • Stupidity corrections

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-export version=0.1.980
+export version=0.1.981
 
 #COLOR OUTPUT FOR RICH OUTPUT 
 ORANGE=$'\e[1;33m'
