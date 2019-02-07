e552a79278550b068ad3090bc414389f60c29b2d
Andrew J Freyer <andrew.freyer@gmail.com>
2019-02-06 20:09:14 -0700

    0.1.973

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-export version=0.1.972
+export version=0.1.973
 
 #COLOR OUTPUT FOR RICH OUTPUT 
 ORANGE=$'\e[1;33m'
