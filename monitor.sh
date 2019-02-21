996ff881bc23fed4e6e2383d9c81e2c7b89239ec
Andrew J Freyer <andrew.freyer@gmail.com>
2019-02-21 16:41:12 -0700

    0.2.020
    • Mqtt retain updates

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-export version=0.2.019
+export version=0.2.020
 
 #COLOR OUTPUT FOR RICH OUTPUT 
 ORANGE=$'\e[1;33m'
