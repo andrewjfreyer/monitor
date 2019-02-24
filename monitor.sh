549f5312570657baa3d9c58582062cac54878a09
Andrew J Freyer <andrew.freyer@gmail.com>
2019-02-23 19:15:18 -0700

    0.2.024
    • Change device tracker post path to
    /topic_path/aa:bb:cc:dd:ee:ff/device_tracker

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-export version=0.2.023
+export version=0.2.024
 
 #COLOR OUTPUT FOR RICH OUTPUT 
 ORANGE=$'\e[1;33m'
