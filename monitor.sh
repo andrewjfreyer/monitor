e6f30cb85aae0f5faced97f8f23f70cbe28fcf4c
Andrew J Freyer <andrew.freyer@gmail.com>
2019-03-19 16:25:26 -0600

    0.2.079
    • Logging reductions continued

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-export version=0.2.078
+export version=0.2.079
 
 #COLOR OUTPUT FOR RICH OUTPUT 
 ORANGE=$'\e[1;33m'
