ad376cb27c8676d3b0b10848719e49398f6398fe
Andrew J Freyer <andrew.freyer@gmail.com>
2019-04-17 09:06:15 -0600

    0.2.163

--- monitor.sh
+++ monitor.sh
@@ -1099,8 +1099,8 @@ while true; do
 			elif [[ $mqtt_topic_branch =~ .*ADD\ DEVICE.* ]]; then 
 
 				if [[ "${data_of_instruction^^}" =~ ([A-F0-9]{2}:){5}[A-F0-9]{2} ]]; then 
-					mac=$(echo "${BASH_REMATCH}" | head -1 )
-					[ "${known_static_addresses[$mac]+abc}" ] && printf "%s\n" "Address: $mac exists" || printf "%s\n" "Address: $mac does not exist" 
+					mac="${BASH_REMATCH}"
+					[ -v known_static_addresses[${BASH_REMATCH}] ] && printf "%s\n" "Address: $mac exists" || printf "%s\n" "Address: $mac does not exist" 
 				fi
 
 			elif [[ $mqtt_topic_branch =~ .*DEPART.* ]]; then 
