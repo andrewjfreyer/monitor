2c3f19269c6ebcc9d185f41d41fedf79c7e9aadc
Andrew J Freyer <andrew.freyer@gmail.com>
2019-05-01 10:12:40 -0600

    0.2.189
    • improved logging clarity for mqtt messaging

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-export version=0.2.188
+export version=0.2.189
 
 #COLOR OUTPUT FOR RICH OUTPUT 
 ORANGE=$'\e[1;33m'
