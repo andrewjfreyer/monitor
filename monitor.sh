eecd24941f8cf1f37c0d5e4833687fa57730247a
Andrew J Freyer <andrew.freyer@gmail.com>
2019-05-01 12:48:57 -0600

    0.2.191
    • mqtt error handling buildout & testing

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-export version=0.2.190
+export version=0.2.191
 
 #COLOR OUTPUT FOR RICH OUTPUT 
 ORANGE=$'\e[1;33m'
