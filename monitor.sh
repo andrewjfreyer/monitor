749c14182c963e4d0a22867a7cf23d2b7580c0d2
Andrew J Freyer <andrew.freyer@gmail.com>
2019-05-02 15:12:40 -0600

    0.2.196
    • mqtt error handling improvements

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-export version=0.2.195
+export version=0.2.196
 
 #COLOR OUTPUT FOR RICH OUTPUT 
 ORANGE=$'\e[1;33m'
