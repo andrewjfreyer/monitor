1deec402b9b6323a964381819b199300259bb584
Andrew J Freyer <andrew.freyer@gmail.com>
2020-01-12 12:07:55 -0700

    0.2.200
    possible fix to fast pipe closures for quicker devices

--- monitor.sh
+++ monitor.sh
@@ -25,7 +25,7 @@
 # ----------------------------------------------------------------------------------------
 
 #VERSION NUMBER
-export version=0.2.199
+export version=0.2.200
 
 #COLOR OUTPUT FOR RICH OUTPUT 
 ORANGE=$'\e[1;33m'
